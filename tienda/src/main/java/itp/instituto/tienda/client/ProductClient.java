cdda55ab0d0d4271c0e94a8d63729f7bb55dd108
maicol910 <maicoljosa910@gmail.com>
2022-11-18 13:08:38 -0500

    arregladosupu

--- tienda/src/main/java/itp/instituto/tienda/client/ProductClient.java
+++ tienda/src/main/java/itp/instituto/tienda/client/ProductClient.java
@@ -3,7 +3,6 @@ package itp.instituto.tienda.client;
 import itp.instituto.tienda.model.Product;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +15,7 @@ public interface ProductClient {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Product> getProduct(@PathVariable("id") Long id);
 
-    @PutMapping(value = "/{id}/stock")
+    @GetMapping(value = "/{id}/stock")
     public ResponseEntity<Product> updateStockProduct(@PathVariable Long id, @RequestParam(name = "quantity", required = true) Double quantity);
 
 }
